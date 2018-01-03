b10ad1ddce5fa4cee94927a01f81e35bce063d9f
Alec German <alec_german@yahoo.com>
2018-01-03 02:07:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array = array.slice(0);
+  array.slice(1);
   return array;
 }
