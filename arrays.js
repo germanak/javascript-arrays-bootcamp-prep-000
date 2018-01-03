746c8e0f010deff7f5415bd8c21d4c07e95a6466
Alec German <alec_german@yahoo.com>
2018-01-03 01:56:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   array = [element, ...array];
   return array;
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+}
